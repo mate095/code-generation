eb007335c4b87396c357d9332052c15608f019e3
zold.mate <mate951006@gmail.com>
2018-10-23 14:10:03 +0200

    CodeDOM basic class generation

---
+++ CodeDOMGenerator/AnimalGenerator.cs
@@ -0,0 +1,58 @@
+﻿namespace CodeGeneration.CodeDOMGenerator
+{
+    using System.IO;
+    using System.CodeDom;
+    using System.CodeDom.Compiler;
+    using CodeGeneration.DSL;
+
+    /// <summary>
+    /// AnimalGenerator creates a graph using a CodeCompileUnit and  
+    /// generates source code for the graph using the CSharpCodeProvider.
+    /// </summary>
+    public class AnimalGenerator
+    {
+        private IAnimal animal;
+
+        private CodeCompileUnit targetUnit;
+        private CodeTypeDeclaration targetClass;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AnimalGenerator(IAnimal animal, CodeCompileUnit targetUnit)
+        {
+            this.animal = animal;
+            this.targetUnit = targetUnit;
+        }
+
+        /// <summary>
+        /// Generate CSharp source code from the compile unit.
+        /// </summary>
+        public void GenerateCSharpCode(string fileName)
+        {
+            CreateGraph();
+
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+            CodeGeneratorOptions options = new CodeGeneratorOptions();
+            options.BracingStyle = "C";
+            using (StreamWriter sourceWriter = new StreamWriter(fileName))
+            {
+                provider.GenerateCodeFromCompileUnit(
+                    targetUnit, sourceWriter, options);
+            }
+        }
+
+        private void CreateGraph()
+        {
+            InitClass();
+        }
+        
+        private void InitClass()
+        {
+            targetClass = new CodeTypeDeclaration(animal.Name);
+            CodeNamespace codeNamespace = new CodeNamespace("CodeGeneration.CodeDOMGenerator");
+            codeNamespace.Types.Add(targetClass);
+            targetUnit.Namespaces.Add(codeNamespace);
+        }
+    }
+}
---
+++ CodeDOMGenerator/Program.cs
@@ -0,0 +1,21 @@
+namespace CodeGeneration.CodeDOMGenerator
+{
+    using System.CodeDom;
+    using CodeGeneration.TestDataCreator;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var creator = new TestDataCreator();
+            var world = creator.CreatWorld();
+
+            foreach(var animal in world.Animals)
+            {
+                AnimalGenerator generator = new AnimalGenerator(animal, new CodeCompileUnit());
+                string outputPath = $"..\\..\\GenerationResult\\_Generated\\_CodeDOM\\{animal.Name}.cs";
+                generator.GenerateCSharpCode(outputPath);
+            }
+        }
+    }
+}
